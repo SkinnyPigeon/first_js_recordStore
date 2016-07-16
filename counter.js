3586ae6f4ee74e33c9d883cf8c3db28a28e150d6
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-16 12:11:16 +0100

    starting to get the counter into place

--- counter.js
+++ counter.js
@@ -1,5 +1,22 @@
 var Counter = function( total ) {
   this.total = total;
+  this.change  = function() {
+    var multiply = 100 * total;
+
+    console.log( "Total: £" + total )
+    console.log( "£10: " + Math.floor( multiply / 1000 ) )
+    console.log( "£5: " + Math.floor( multiply % 1000 / 500 ) )
+    console.log( "£2: " + Math.floor( multiply % 1000 % 500 / 200 ) )
+    console.log( "£1: " + Math.floor( multiply % 1000 % 500 % 200 / 100 ) )
+    console.log( "50p: " + Math.floor( multiply % 1000 % 500 % 200 % 100 / 50 ) )
+    console.log( "20p: " + Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 / 20 ) )
+    console.log( "10p: " + Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 / 10 ) )
+    console.log( "5p: " + Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 % 10 / 5 ) )
+    console.log( "2p: " + Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5 / 2) )
+    console.log( "1p: " + Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5 % 2 / 1 ) )
+  }
 }
 
+counter = new Counter( 72.58 );
+counter.change()
 module.exports = Counter;
--- specs/counter_spec.js
+++ specs/counter_spec.js
@@ -4,11 +4,12 @@ var assert = require( 'chai' ).assert;
 
 describe( "The Counter: ", function() {
   beforeEach( function() {
-    counter = new Counter( 1000 );
+    counter = new Counter( 19.52 );
   })
 
   it( "Should return the total", function() {
-    assert.equal( 1000, counter.total )
+    // counter.total( 19.52 )
+    assert.equal( 19.52, counter.total )
   })
 
  
